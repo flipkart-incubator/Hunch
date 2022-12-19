fe674d182beb6ad9d75dfb33dae975a92efdaba7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-19 08:47:28 +0000

    Adding tarfile member sanitization to extractall()

--- hunch_server/model_loader.py
+++ hunch_server/model_loader.py
@@ -88,7 +88,26 @@ class ModelLoader:
 
         prediction_module_tar_contents = model_obj['model_predict_module']
         with TarFile.open(fileobj=BytesIO(prediction_module_tar_contents), mode='r:bz2') as tar:
-            tar.extractall(prediction_module_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, prediction_module_path)
         os.rename(prediction_module_path +'/model.py', prediction_module_path +'/' + self.__prediction_module.format(model_id=model_id, model_version=model_version.replace('.', '_')) + '.py')
 
     def __extract_model_resources(self, model_obj, model_id, model_version):
@@ -98,7 +117,26 @@ class ModelLoader:
 
         model_resources_tar_contents = model_obj['modeldir_blob']
         with TarFile.open(fileobj=BytesIO(model_resources_tar_contents), mode='r:bz2') as tar:
-            tar.extractall(model_resource_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, model_resource_path)
 
     def ___model_resource_path(self, model_id, model_version):
         return self.__model_resource_path.format(model_id=model_id, model_version=model_version)
